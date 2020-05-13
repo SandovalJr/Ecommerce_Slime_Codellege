19f5ca9a1092904cd2307cc722cc31bef9b8336d
SandovalJr <edubizzle7@gmail.com>
2020-05-13 09:17:44 -0500

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/home/shared/navbar/navbar.component';
+import { NotFoundComponent } from './components/home/shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule
