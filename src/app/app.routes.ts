c525d23bfdb23da5e09342145e4de0ce08648468
SandovalJr <edubizzle7@gmail.com>
2020-05-14 11:26:55 -0500

    rutas compontes agregadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,19 @@ import { NotFoundComponent } from './../app/components/home/shared/not-found/not
 import { AboutComponent } from './components/home/about/about.component';
 import { ContactComponent } from './components/home/contact/contact.component';
 
+// TIENDA COMPONENTES
+import { CaballeroComponent } from './components/home/products/caballero/caballero.component';
+import { DamaComponent } from './components/home/products/dama/dama.component';
+import { KidsComponent } from './components/home/products/kids/kids.component';
+
 const rutas: Routes = [
   { path: '', component: InicioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  // TIENDA
+  { path: 'caballero', component: CaballeroComponent },
+  { path: 'dama', component: DamaComponent },
+  { path: 'kids', component: KidsComponent },
   // SIEMPRE RUTA DEFAULT
   { path: 'notFound', component: NotFoundComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'notFound' },
