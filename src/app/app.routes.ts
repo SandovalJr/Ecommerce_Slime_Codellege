8eaeecee1cf7b044e693ae5682835d58c3ba5b67
SandovalJr <edubizzle7@gmail.com>
2020-05-13 22:17:57 -0500

    se establecio ruta de contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './components/home/inicio/inicio.component';
 import { NotFoundComponent } from './../app/components/home/shared/not-found/not-found.component';
 import { AboutComponent } from './components/home/about/about.component';
+import { ContactComponent } from './components/home/contact/contact.component';
 
 const rutas: Routes = [
   { path: '', component: InicioComponent },
   { path: 'about', component: AboutComponent },
-
+  { path: 'contact', component: ContactComponent },
   // SIEMPRE RUTA DEFAULT
   { path: 'notFound', component: NotFoundComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'notFound' },
